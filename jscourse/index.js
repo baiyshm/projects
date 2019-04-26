cde8053f65cd17b7db7a61429e6b0a80190b0b2f
Baiysh <baiyshm@gmail.com>
2019-04-26 16:20:28 +0600

    The my first commit

---
+++ jscourse/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var a=prompt("Are you 18?", "Yes");
+var massiv = ['23423432','32432423423'];
+console.log(a);
+console.log(typeof(massiv));
---
+++ jscourse/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+var a='string';
+var b=' 9';
+console.log(a+b);
